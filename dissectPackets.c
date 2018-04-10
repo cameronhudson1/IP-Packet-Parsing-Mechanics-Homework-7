c4e03afbac5a29a528962cb1af040b416102ebbb
Cameron Edward Hudson <cmrnhudson@gmail.com>
2018-04-10 09:38:30 -0400

    Initial Commit w/ basic structures

---
+++ dissectPackets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*
+Program to parse data from a text file including IP packet data
+
+Parse:
+1. Version
+2. IHL (Header Length)
+3. Type of Service (TOS)
+4. Total Length
+5. Identification
+6. IP Flags(**note**)
+7. Fragment Offset
+8. Time To Live (TTL)
+9. Protocol
+10. Header Checksum
+11. Source Address
+12. Destination Address
+
+*/
+
+void parseData(){
+
+
+}
+
+int main(int argc, char **argv){
+
+}
