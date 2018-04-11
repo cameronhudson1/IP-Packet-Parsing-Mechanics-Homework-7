b4d09b80a2e119907be44ed045caf8a0d07206bd
Cameron Edward Hudson <cmrnhudson@gmail.com>
2018-04-10 23:53:13 -0400

    Did some stuff with the bit fields and making stuff kind of work

--- dissectPackets.c
+++ dissectPackets.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define _BSD_SOURCE
+#include <stdint.h>
+#include <endian.h>
+
+
 /*
 Program to parse data from a text file including IP packet data
 
@@ -21,11 +26,59 @@ Parse:
 
 */
 
-void parseData(){
+typedef struct packetData{
+	unsigned char version:4;
+	unsigned char ihl:4;
+	unsigned char tos:8;
+	unsigned int totalLength:16;
+	unsigned int identification:16;
+	unsigned char flags:3;
+	unsigned int fragmentOffset:13;
+	unsigned char ttl:8;
+	unsigned char protocol:8;
+	unsigned int headerChecksum:16;
+	unsigned int sourceAddress:16;
+	unsigned char* ipAddress;
+} packetData;
 
+void printPacket(packetData* packet, number){
+	printf("==>Packet %d", number);
+}
 
+packetData* parsePacket(FILE* fp){
+	packetData* packet = malloc(sizeof(packetData));
+	
+}
+
+packetData** parseData(FILE* fp){
+	//Get number of packets
+	int packets;
+	fread(&packets, 4, 1, fp);
+	
+	//Make an array to hold pointers to all packets
+	packetData** allPackets = malloc(packets);
+	
+	for(int i = 0; i < packets; i++){
+		allPackets[i] = parsePacket(fp);
+	}
+	
+	for(int i = 0; i < packets; i++){
+		printPacket(allPackets[i], i);
+	}
 }
 
 int main(int argc, char **argv){
-
+	if(argc > 1 && argc < 3){		//1 command line args
+		FILE *fp;
+		if((fp = fopen(argv[1], "rb")) == NULL){
+			packetData** packets = parseData(fp);
+		}
+		else{
+			fprintf(stderr, "An invalid file was input\n");
+		}		
+	}
+	else{							//Not enough or too many command line args
+		fprintf(stderr, "Invalid number of command line arguments\n");
+	}
+	return 0;
 }
